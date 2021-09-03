bf3b61c107ef837aa245492546caaf926e4d7c5a
Amanda Morgan <AmandaC0022@gmail.com>
2021-09-03 09:39:22 -0500

    added basic functions to server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js');
+
+//allows for Heroku port or my own at 3001
+const PORT = process.env.PORT || 3001;
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
